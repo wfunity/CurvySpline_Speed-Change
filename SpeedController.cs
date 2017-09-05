fa8b126cc0baf3f7964735f4b582951679fb6e59
MarkGT <wfunity@163.com>
2017-09-05 13:16:35 +0800

    curvrSpline 路径插件 ，调整每个节点速度值； 脚本拖到 节点上即可

---
+++ SpeedController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FluffyUnderware.Curvy.Controllers;
+
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
+public class SpeedController : MonoBehaviour
+{
+    private SplineController splineCtroller;
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+        splineCtroller = FindObjectOfType<SplineController>();
+        if (splineCtroller == null)
+        {
+            Debug.Log("SplineController is Null!");
+            return;
+        }
+        GetComponent<BoxCollider>().isTrigger = true;
+        GetComponent<Rigidbody>().isKinematic = true;
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (splineCtroller == null)
+        {
+            // Debug.Log("SplineController is Null!");
+            return;
+        }
+        if (other.tag == "Player")
+        {
+            splineCtroller.Speed = speed;
+        }
+    }
+}
